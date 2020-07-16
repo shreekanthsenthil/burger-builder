536ea9f0c2fc8a87a66ebd752de872027979c7f6
shreekanth senthil <shreerushmi@gmail.com>
2020-07-16 16:14:08 +0530

    Responsive NavBar

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,19 +1,38 @@
-import React from 'react'
+import React, {Component} from 'react'
 
 import Aux from '../../hoc/Auxiliary'
 import classes from './Layout.module.css'
 import Toolbar from '../Navigation/Toolbar/Toolbar'
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
 
-const layout = (props) => (
-    <Aux>
-    <div>
-        <Toolbar></Toolbar>
-        
-    </div>
-    <main className={classes.Content}>
-        {props.children}
-    </main>
-    </Aux>
-)
+class Layout extends Component {
+    state = {
+        showSideDrawer: false
+    }
+    
+    sideDrawerClosedHandler = () => {
+        this.setState({showSideDrawer: false})
+    }
 
-export default layout
+    sideDrawerToggleHandler = () => {
+
+        this.setState(prevState => {
+            return {showSideDrawer: !prevState.showSideDrawer}
+        })
+    }
+
+    render () { 
+        return (
+            <Aux>
+            <div>
+                <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler}/>
+                <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerClosedHandler}/>
+                
+            </div>
+            <main className={classes.Content}>
+                {this.props.children}
+            </main>
+            </Aux>
+        )}
+}
+export default Layout
